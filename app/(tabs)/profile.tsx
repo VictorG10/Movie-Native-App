12ed1fd9d9da812700daa4d1875d738ff8b5d949
VictorG10 <victorgbadebo10@gmail.com>
2025-05-23 13:08:54 +0100

    movie details screen

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,12 +1,15 @@
+import { icons } from "@/constants/icons";
 import React from "react";
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Image, Text, View } from "react-native";
 
 const Profile = () => {
   return (
-    <SafeAreaView>
-      <Text>Profile</Text>
-    </SafeAreaView>
+    <View className="bg-primary flex-1 px-10">
+      <View className="flex justify-center items-center flex-1 flex-col gap-5">
+        <Image source={icons.arrow} className="size-10" tintColor="#fff" />
+        <Text className="text-gray-500 text-base">Profile</Text>
+      </View>
+    </View>
   );
 };
 
